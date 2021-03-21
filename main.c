54f3404d488f4ea1cae64f4a050f7b22362e6e2a
luciolemos <luciolemos.j5@gmail.com>
2021-03-21 10:22:09 -0300

    Medias em linguagem C de programação

---
+++ main.c
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: luciolemos
+ *
+ * Created on 21 de março de 2021, 10:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gconio.h> //Necessária para as funções: clrscr(), gotoxy(),  delay()
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("CURSO: CIÊNCIA DA COMPUTAÇÃO\n");
+    delay(1000); //espera 2 segundos
+
+    printf("DISCIPLINA: TÉCNICAS DE PROGRAMAÇÃO\n");
+    delay(1000); //espera 2 segundos
+
+    printf("ATIVIDADE: 1\n");
+    delay(1000); //espera 2 segundos
+
+    printf("ALUNO: LÚCIO FLÁVIO LEMOS\n");
+    delay(1000); //espera 2 segundos
+
+    printf("MATRÍCULA: 2019204050\n");
+    delay(1000); //espera 2 segundos
+
+    float A1, A2, A3, A4, N1;
+    printf("Digite a Nota da Atividade 1: ");
+    scanf("%f", &A1);
+
+    printf("Digite a Nota da Atividade 2: ");
+    scanf("%f", &A2);
+
+    printf("Digite a Nota da Atividade 3: ");
+    scanf("%f", &A3);
+
+    printf("Digite a Nota da Atividade 4: ");
+    scanf("%f", &A4);
+
+    printf("As notas declaradas foram %.2f, %.2f, %.2f e %.2f\n", A1, A2, A3, A4);
+    N1 = (A1 + A2 + A3 + A4) / 4;
+    if (N1 >= 6) {
+        printf("Nota N1 do aluno: %.2f\n", N1);
+        printf("Aluno aprovado!\n");
+    } else {
+        printf("Nota N1 do aluno: %.2f\n", N1);
+        printf("Aluno Reprovado!\n");
+    }
+    system("PAUSE");
+    return (EXIT_SUCCESS);
+}
+
